12e279fb4fc98b0f748450cc66ca27754be7a4e9
tsironneau <tanguy.sironneau@gmail.com>
2019-03-24 12:15:15 +0100

    feat (puzzle) : add Horse Race easy puzzle java solution
    See also :
    https://www.codingame.com/ide/puzzle/horse-racing-duals

---
+++ puzzles/easy/HorseRace.java
@@ -0,0 +1,37 @@
+package easy;
+
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+/*
+https://www.codingame.com/ide/puzzle/horse-racing-duals
+ */
+public final class HorseRace {
+
+    public static void main(final String[] args) {
+        final Scanner in = new Scanner(System.in);
+        final int N = in.nextInt();
+
+        final List<Integer> powerList = IntStream.range(0, N)
+                .map(i -> in.nextInt())
+                .boxed()
+                .sorted()
+                .collect(Collectors.toList());
+
+        int minDiff = Integer.MAX_VALUE;
+        for (int i = 0; i < powerList.size() - 1; i++) {
+            final int current = powerList.get(i);
+            final int next = powerList.get(i + 1);
+            final int currentDiff = next - current;
+            if (minDiff > currentDiff) {
+                minDiff = currentDiff;
+                if (minDiff == 0)
+                    break;
+            }
+        }
+
+        System.out.println(minDiff);
+    }
+}
